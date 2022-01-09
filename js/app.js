3aa5d245f22e4939459f83e40e87a1b83a948bb0
erikawww <welschenerika@gmail.com>
2022-01-09 02:26:40 -0300

    cambios en footer,gitignore,hamburger menu + email submission

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 // NAVBAR
 // abrir y cerrar menú
 const navSlide = () => {
-  const burger = document.querySelector('.cabecera__btn');
-  const closeIcon = document.querySelector(".cabecera__btn--close");
-  const openIcon = document.querySelector('.cabecera__btn--menu')
+  const burger = document.querySelector('.hamburger');
   const cabecera = document.querySelector('.cabecera');
   const menu = document.querySelector('.navbar__menu');
   const titulo = document.querySelector('.cabecera__titulo a');
@@ -12,19 +10,15 @@ const navSlide = () => {
 
 
   burger.addEventListener('click', () => {
-    cabecera.classList.toggle('active')
+    burger.classList.toggle('is-active');
+    cabecera.classList.toggle('active');
     menu.classList.toggle('active');
     if (cabecera.classList.contains('active')) {
-          closeIcon.style.display = "block";
-          openIcon.style.display = "none";
           titulo.style.color ="#f8f6f1";
-          closeIcon.style.color = "#f8f6f1"
-          cabecera.style.color = "#ff6347"
+          cabecera.style.color = "#ff6347";
           mainContent.style.zIndex = "-1";
           galeria.style.zIndex = -"-1";
     } else {
-          closeIcon.style.display = "none";
-          openIcon.style.display = "block";
           titulo.style.color ="#323232";
           mainContent.style.zIndex = "1";
           galeria.style.heigth = "100px";
@@ -34,6 +28,7 @@ const navSlide = () => {
 
 }
 navSlide();
+
 
 
 // Galeria baguette box 
